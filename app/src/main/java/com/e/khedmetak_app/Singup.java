6515986bed94c4bebeea61a5ae54187935ae3e31
yousef_a_z <khedmetakapp@gmail.com>
2020-05-01 10:46:06 -0700

    signup activity

---
+++ app/src/main/java/com/e/khedmetak_app/Singup.java
@@ -0,0 +1,14 @@
+package com.e.khedmetak_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Singup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+    }
+}
